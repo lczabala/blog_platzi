28c4207b03d1c1bafca6d39f975aaa943e465d30
Luis Zabala <lzabala28@gmail.com>
2019-07-16 19:00:19 -0400

    se logra cambiar el open de la publicación

--- src/actions/publicationsAction.js
+++ src/actions/publicationsAction.js
@@ -87,21 +87,20 @@ export const userPublications = (key) => async(dispatch, getState) =>{
 export const openClosePublications = (key, comment_key) => (dispatch, getState) => {
     const {publications} = getState().userPublicationsReducer
     // Se identifica qué publicación fue la seleccionada
-    const publicacionsUpdated = [...publications]
     const publicacionSelected = publications[key][comment_key]
-    console.log(publicacionSelected)
+  
     const openPublicacionUptadeted = {
         ...publicacionSelected,
         open: !publicacionSelected.open
     }
-    console.log(openPublicacionUptadeted) 
     
-    console.log(publicacionsUpdated)
+    const publicacionsUpdated = [...publications]
+   
     publicacionsUpdated[key] = [
         ...publications[key]
     ]
-    console.log(publicacionsUpdated[key])
-    publicacionsUpdated[key][comment_key] = openClosePublications
+    
+    publicacionsUpdated[key][comment_key] = openPublicacionUptadeted
     
     dispatch({        
         type: BRING_PUBLICATIONS,
